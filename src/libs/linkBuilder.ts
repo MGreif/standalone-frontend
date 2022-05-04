c80faf10aa9d55a3c1da4e637fc2b93bedab37b9
Mika Greif <mika.greif@greenbone.net>
2022-05-04 21:05:09 +0200

    Add NGINX PATH env var

--- src/libs/linkBuilder.ts
+++ src/libs/linkBuilder.ts
@@ -1,2 +1,2 @@
 export const buildApiLink = (url = '') => `${process.env.REACT_APP_USER_SERVICE_HOST}:${process.env.REACT_APP_USER_SERVICE_PORT}/api${process.env.REACT_APP_USER_SERVICE_PATH}${url}`
-export const buildRouterLink = (url = '') => `${process.env.REACT_APP_USER_SERVICE_PATH}${url}`
+export const buildRouterLink = (url = '') => `${process.env.REACT_APP_NGINX_PATH}${url}`
